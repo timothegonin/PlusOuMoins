4e832557336f32a592b34bd24a48718d8c30e09d
timotheG <gonin.timothe@gmail.com>
2024-01-02 11:35:13 +0100

    number entry

--- main.c
+++ main.c
@@ -8,9 +8,17 @@ int main()
     int nombreMystere = 0;
     const int MAX = 100, MIN = 1;
 
+    //initialisation du nombre
     srand(time(NULL));
     nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
 
-    printf("TEST - nombre mystere est = %d", nombreMystere);
+    int nombreEntre = 0;
+    printf("Saisir un nombre : \n");
+    scanf("%d", &nombreEntre);
+
+
+    //retour
+    printf("TEST - nombre entre est = %d\n", nombreEntre);
+    printf("TEST - nombre mystere est = %d\n", nombreMystere);
     return 0;
 }
